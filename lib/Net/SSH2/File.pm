72d979a2603b0ad324006e866ead2f2c561834f2
Salvador Fandiño <sfandino@yahoo.com>
2015-09-28 22:52:25 +0200

    improve documentation for Net::SFTP::File::write

--- lib/Net/SSH2/File.pm
+++ lib/Net/SSH2/File.pm
@@ -68,7 +68,7 @@ __END__
 
 =head1 NAME
 
-Net::SSH2::File - SSH 2 SFTP file object
+Net::SSH2::File - SSH2 SFTP file object
 
 =head1 DESCRIPTION
 
@@ -81,7 +81,43 @@ read, or undef on failure.
 
 =head2 write ( buffer )
 
-Write buffer to the remote file; returns bytes written, undef on failure.
+Write buffer to the remote file.
+
+The C<libssh2_sftp_write> function wrapped by this method has a
+complex and quite difficult (if not impossible at all) to use API. It
+tries to hide the packet pipelining being done under the hood in order
+to attain decent throughput.
+
+Net::SSH2 can not hide that complexity without negatively affecting
+the transmission speed so it provides just a thin wrapper for that
+library function.
+
+An excerpt from C<libssh2_sftp_write> manual page follows:
+
+  WRITE AHEAD
+
+  Starting in libssh2 version 1.2.8, the default behavior of libssh2
+  is to create several smaller outgoing packets for all data you pass
+  to this function and it will return a positive number as soon as the
+  first packet is acknowledged from the server.
+
+  This has the effect that sometimes more data has been sent off but
+  isn't acked yet when this function returns, and when this function
+  is subsequently called again to write more data, libssh2 will
+  immediately figure out that the data is already received remotely.
+
+  In most normal situation this should  not cause any problems, but it
+  should be noted that if you've once called libssh2_sftp_write() with
+  data and  it returns short, you  MUST still assume that  the rest of
+  the data  might've been cached  so you need  to make sure  you don't
+  alter that  data and think  that the version  you have in  your next
+  function invoke will be detected or used.
+
+  The reason  for this funny behavior  is that SFTP can  only send 32K
+  data in each packet and it gets all packets acked individually. This
+  means we  cannot use a  simple serial approach  if we want  to reach
+  high performance even on high latency connections. And we want that.
+
 
 =head2 stat
 
@@ -103,16 +139,20 @@ Returns the current file pointer offset.
 
 L<Net::SSH2::SFTP>.
 
-=head1 AUTHOR
-
-David B. Robins, E<lt>dbrobins@cpan.orgE<gt>
-
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2005, 2006 by David B. Robins; all rights reserved.
+Copyright (C) 2005, 2006 by David B. Robins E<lt>dbrobins@cpan.orgE<gt>;
+
+Copyright (C) 2015 by Salvador FandiE<ntilde>o E<lt>sfandino@yahoo.comE<gt>;
+
+All rights reserved.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.0 or,
 at your option, any later version of Perl 5 you may have available.
+
+The documentation for this package contains and excerpt from libssh2
+manual pages. You can consult the license of the libssh2 project for
+the conditions regulating the copyright of that part.
 
 =cut
