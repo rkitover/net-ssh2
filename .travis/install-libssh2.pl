ca220998add8a7fdd3beaa1083e9a715829dd2a6
Salvador Fandino <sfandino@yahoo.com>
2016-04-28 11:31:48 +0200

    Travis-CI: check also againt the development branch of libssh2

--- .travis/install-libssh2.pl
+++ .travis/install-libssh2.pl
@@ -8,24 +8,31 @@ unless ($ENV{TRAVIS}) {
     die "This script is only intended to be run from Travis CI platform\n";
 }
 
-my $workdir = "footomizeme";
-my $libssh2_ver = shift || "1.7.0";
-my $tgz_dir = "libssh2-$libssh2_ver";
-my $tgz_name = "$tgz_dir.tar.gz";
-my $tgz_url = "https://www.libssh2.org/download/libssh2-$libssh2_ver.tar.gz";
-my $cflags = "-g -O0";
 my $prefix = "libssh2";
-
+my $workdir = "footomizeme";
 $prefix = File::Spec->rel2abs($prefix);
-
 system "rm -Rf $prefix" and die "execution of 'rm -Rf $prefix' failed, RC: $?";
 system "rm -Rf $workdir";
 mkdir $workdir or die $!;
 chdir $workdir or die $!;
 
-for (1..5) {
-    system "wget --retry-connrefused --tries=8 --no-check-certificate $tgz_url" or last;
+my $cflags = "-g -O0";
+
+my $libssh2_ver = shift || "1.7.0";
+if ($libssh2_ver eq 'git') {
+    system "git clone https://github.com/libssh2/libssh2.git" and die;
+    chdir "libssh2" or die $!;
+    system "./buildconf";
 }
-system "tar xf $tgz_name" and die;
-chdir $tgz_dir or die $!;
+else {
+    my $tgz_dir = "libssh2-$libssh2_ver";
+    my $tgz_name = "$tgz_dir.tar.gz";
+    my $tgz_url = "https://www.libssh2.org/download/libssh2-$libssh2_ver.tar.gz";
+
+    for (1..5) {
+        system "wget --retry-connrefused --tries=8 --no-check-certificate $tgz_url" or last;
+    }
+    system "tar xf $tgz_name" and die;
+    chdir $tgz_dir or die $!;
+}
 system "CFLAGS='$cflags' ./configure --prefix=$prefix --enable-shared=yes --enable-static=no --disable-examples-build && make && make install" and die $?;
